ef3ab3fff2c2681393512f4c21774a2f5f7168e8
SekaiNiwaka <kantaro061026@outlook.jp>
2025-03-04 18:47:37 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask_asgi import FlaskAsgi, render_template, request, redirect
 import sqlite3
 
-app = Flask(__name__)
+app = FlaskAsgi(__name__)
 
 # SQLiteデータベースのセットアップ
 DATABASE = 'text_data.db'
